8d65fe946161c324ecaa81c086e87948ad2a9311
IntPtr <lxy.lixiaoyan@gmail.com>
2017-09-24 01:07:38 +0800

    update

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@
 
 inkscape ./images/section-projects.svg -d 384 -e ./images/section-projects.png
 inkscape ./images/section-members.svg -d 384 -e ./images/section-members.png
+inkscape ./images/section-contacts.svg -d 384 -e ./images/section-contacts.png
